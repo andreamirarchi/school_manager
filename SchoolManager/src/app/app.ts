96d8ec3dfc8817d0b1be8632a8eb489c19f1865c
andreamirarchi <andrea.mirarchi03@gmail.com>
2025-11-27 12:41:22 +0100

    Base per il progetto

---
+++ SchoolManager/src/app/app.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-voti',
+  standalone: true,
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class App {}
