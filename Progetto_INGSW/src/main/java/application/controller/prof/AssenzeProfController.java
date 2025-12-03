d74dada139b485dd713e1e16f3723ed851430905
Domenico Santagada <115139470+domenicosantagada@users.noreply.github.com>
2025-12-03 16:26:01 +0100

    Caricamento bozza

---
+++ Progetto_INGSW/src/main/java/application/controller/prof/AssenzeProfController.java
@@ -0,0 +1,25 @@
+package application.controller.prof;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class AssenzeProfController {
+    public void mesePrecedente(MouseEvent mouseEvent) {
+    }
+
+    public void meseSuccessivo(MouseEvent mouseEvent) {
+    }
+
+    public void showAddAssenzaPane(ActionEvent actionEvent) {
+    }
+
+    public void backButtonClicked(MouseEvent mouseEvent) {
+    }
+
+    public void addAssenzaClicked(ActionEvent actionEvent) {
+    }
+
+    public void backUpdateClicked(MouseEvent mouseEvent) {
+
+    }
+}
---
+++ RegistroElettronicoDomenico/src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module com.example.registroelettronico {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires fontawesomefx;
+    requires itextpdf;
+    requires jbcrypt;
+
+    opens application to javafx.fxml;
+    exports application;
+
+    exports application.controller;
+    opens application.controller to javafx.fxml;
+    exports application.controller.prof;
+    opens application.controller.prof to javafx.fxml;
+    exports application.controller.studente;
+    opens application.controller.studente to javafx.fxml;
+}
